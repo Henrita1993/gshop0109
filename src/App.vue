e95b34b56ccf89b51e4bc0d49e23eef202adfc2e
lyt <913455233@qq.com>
2021-01-09 13:37:03 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,12 @@
 
 <script>
 import FooterGuide from "../src/components/FooterGuide/FooterGuide"
+import {reqFoodTypes} from "./api/index"
 export default {
+  async mounted(){
+    const result = await reqFoodTypes()
+    console.log(result)
+  },
   components:{
     FooterGuide
   }
